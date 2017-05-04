d89a42195bf69f85df83d660a3241159bfc22018
Attila Simon <sattila83@gmail.com>
2017-05-04 17:24:43 +0100

    Added movement related classes

--- motor.py
+++ motor.py
@@ -1,10 +1,47 @@
 #!/usr/bin/env python
 
-from position import Position
+from pwmobject import PWMObject
+from random import randint
 
-class Motor:
-	def bypass(self):
-		print("Bypassing")
-		
-	def moveToward(self, targetPosition):
-		print("Moving toward %s" % (targetPosition))
+class MotorFunctions:
+	def dodge(self):
+		print("Bypassing obstacle")
+		drive = PWMObject(21) # port 21 for driving
+		turn = PWMObject(20) # port 20 for turning
+		drive.toNegative()
+		left = random.randint(0,1)
+		if left:
+			turn.toPositive()
+		else:
+			turn.toNegative()
+		drive.toPositive()
+		if not left:
+			turn.toPositive()
+		else:
+			turn.toNegative()
+		drive.cleanup()
+		turn.cleanup()
+
+	def moveForward(self):
+		print("Moving forward")
+		drive = PWMObject(21) # port 21 for driving
+		drive.toPositive()
+		drive.cleanup()
+
+	def moveBackward(self):
+		print("Moving forward")
+		drive = PWMObject(21) # port 21 for driving
+		drive.toNegative()
+		drive.cleanup()
+
+	def turnLeft(self):
+		print("Turning left")
+		turn = PWMObject(20) # port 20 for turning
+		turn.toPositive()
+		turn.cleanup()
+
+	def turnRight(self):
+		print("Turning right")
+		turn = PWMObject(20) # port 20 for turning
+		turn.toNegative()
+		turn.cleanup()
---
+++ pwmobject.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+import time
+
+SECONDS = 0.1 # maybe it would be better to have this as a parameter
+
+class PWMObject:
+	def __init__(self, port):
+		self.port = port
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(port, GPIO.OUT)
+		self.pwm = GPIO.PWM(self.port,50) # 50 Hz (20 ms) - it would be nice to have it as a parameter
+
+	def toPositive(self):
+		self.pwm.start(7.5)
+		for i in range(75,100):
+			self.pwm.ChangeDutyCycle(i/10.0)
+			time.sleep(SECONDS)
+		for i in range(100,75,-1):
+			self.pwm.ChangeDutyCycle(i/10.0)
+			time.sleep(SECONDS)
+		self.pwm.ChangeDutyCycle(7.5)
+		self.pwm.stop()
+
+	def toNegative(self):
+		self.pwm.start(7.5)
+		for i in range(75,50,-1):
+			self.pwm.ChangeDutyCycle(i/10.0)
+			time.sleep(SECONDS)
+		for i in range(50,75):
+			self.pwm.ChangeDutyCycle(i/10.0)
+			time.sleep(SECONDS)
+		self.pwm.ChangeDutyCycle(7.5)
+		self.pwm.stop()
+	def cleanup(self):
+		GPIO.cleanup()
