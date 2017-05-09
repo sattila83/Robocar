2ccbb7fedc32aa97ba6b07c154efa5d326c87185
Attila Simon <sattila83@gmail.com>
2017-05-09 11:42:19 +0100

    Implemented logic for PositioningService class

--- position.py
+++ position.py
@@ -6,8 +6,8 @@ EARTH_RADIUS = 6371e3;
 
 class Position:
 	def __init__(self, lat = 0.0, lon = 0.0):
-		self.lat = math.radians(lat)
-		self.lon = math.radians(lon)
+		self.lat = math.radians(float(lat))
+		self.lon = math.radians(float(lon))
 
 	def __repr__(self):
 		return "(%(lat)f,%(lon)f)" % { 'lat': self.lat, 'lon': self.lon }
@@ -15,14 +15,25 @@ class Position:
 	def __str__(self):
 		return "(%(lat)f,%(lon)f)" % { 'lat': math.degrees(self.lat), 'lon': math.degrees(self.lon) }
 
+	@staticmethod
+	def toDecimal(value, direction):
+		parts = value.split('.')
+		decimal = float("0." + parts[1]) / 60.0 # convert seconds
+		decimal = decimal + (float(parts[0][-2:]) / 60.0) # convert minutes
+		decimal = decimal + float(parts[0][:-2]) # convert hours
+		if 'N' == direction or 'E' == direction:
+			return decimal
+		else:
+			return 0.0 - decimal
+
 	def distanceTo(self, otherPosition):
-		deltaLat = otherPosition.lat-self.lat
-		deltaLon = otherPosition.lon-self.lon
-		squareOfHalfChordLength = math.sin(deltaLat/2) * math.sin(deltaLat/2) + math.cos(self.lat) * math.cos(otherPosition.lat) * math.sin(deltaLon/2) * math.sin(deltaLon/2)
-		return EARTH_RADIUS * 2 * math.atan2(math.sqrt(squareOfHalfChordLength), math.sqrt(1-squareOfHalfChordLength))
+		deltaLat = otherPosition.lat - self.lat
+		deltaLon = otherPosition.lon - self.lon
+		squareOfHalfChordLength = math.sin(deltaLat / 2) * math.sin(deltaLat / 2) + math.cos(self.lat) * math.cos(otherPosition.lat) * math.sin(deltaLon / 2) * math.sin(deltaLon / 2)
+		return EARTH_RADIUS * 2 * math.atan2(math.sqrt(squareOfHalfChordLength), math.sqrt(1 - squareOfHalfChordLength))
 
 	def bearingTo(self, otherPosition):
-		deltaLon = otherPosition.lon-self.lon
+		deltaLon = otherPosition.lon - self.lon
 		beeringInRadians = math.atan2(math.sin(deltaLon) * math.cos(otherPosition.lat), math.cos(self.lat) * math.sin(otherPosition.lat) - math.sin(self.lat) * math.cos(otherPosition.lat) * math.cos(deltaLon))
 		return (math.degrees(beeringInRadians) + 360.0) % 360.0
 
--- positioning_service.py
+++ positioning_service.py
@@ -1,11 +1,28 @@
+#!/usr/bin/env python
 
+import time
+
+from position import Position
+from serial_reader import SerialReader
+
+from serial_reader import MODE_GPS_FIX_DATA
+
+# initial values of constants need to be refined according to experience
+# or they can be input variables, if that is considered to be better
+NUMBER_OF_SAMPLES = 30.0
+SAMPLING_TIME_SECONDS = 1
 
 class PositioningService:
-    
-    def __init__(self):
-        None
-    
-    def getActualPosition(self):
-        return None
-    
-    
+
+	@staticmethod
+	def getActualPosition():
+		reader = SerialReader()
+		latSum = 0.0
+		lonSum = 0.0
+		for i in range(0,NUMBER_OF_SAMPLES):
+			data = reader.read(MODE_GPS_FIX_DATA)
+			parts = [part.strip() for part in data.split(',')]
+			latSum = latSum + Position.toDecimal(parts[2], parts[3])
+			lonSum = lonSum + Position.toDecimal(parts[4], parts[5])
+			time.sleep(SAMPLING_TIME_SECONDS)
+		return Position(latSum / NUMBER_OF_SAMPLES, lonSum / NUMBER_OF_SAMPLES)
