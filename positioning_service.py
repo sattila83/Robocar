71db2cbb00e79cca0f057b3a7e5e95fbf0aef90e
Attila Simon <sattila83@gmail.com>
2017-05-24 12:35:40 +0100

    Refined PositioningService class according to experience

--- positioning_service.py
+++ positioning_service.py
@@ -11,14 +11,22 @@ class PositioningService:
 
 	@staticmethod
 	# initial values of input params need to be refined according to experience
-	def getActualPosition(numberOfSamples = 30, samplingTimeInSeconds = 1.0):
+	def getActualPosition(numberOfSamples = 5, samplingTimeInSeconds = 1.0):
+		print "Reading GPS data for " + numberOfSamples * samplingTimeInSeconds + " seconds"
 		reader = SerialReader()
 		latSum = 0.0
 		lonSum = 0.0
+		validSampleCount = 0
 		for i in range(0, numberOfSamples):
 			data = reader.read(MODE_GPS_FIX_DATA)
 			parts = [part.strip() for part in data.split(',')]
-			latSum = latSum + Position.toDecimal(parts[2], parts[3])
-			lonSum = lonSum + Position.toDecimal(parts[4], parts[5])
+			if len(parts) > 5 and len(parts[2]) > 0 and len(parts[3]) > 0 and len(parts[4]) > 0 and len(parts[5]) > 0:
+				validSampleCount += 1
+				latSum = latSum + Position.toDecimal(parts[2], parts[3])
+				lonSum = lonSum + Position.toDecimal(parts[4], parts[5])
+			else:
+				print data
 			time.sleep(samplingTimeInSeconds)
-		return Position(latSum / numberOfSamples, lonSum / numberOfSamples)
+		if validSampleCount == 0:
+			return Position(0, 0)
+		return Position(latSum / validSampleCount, lonSum / validSampleCount)
